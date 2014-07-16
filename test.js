24969a0359fee6ba6e65955bba1d82a5c15cb82f
Admin Glassful <Kathleen@Kathleens-iMac.local>
2014-07-16 13:37:32 -0400

    second one

--- test.js
+++ test.js
@@ -1,5 +1,16 @@
 var MyTestPage = require('./File.js');
 
-MyTestPage.Page.build()
-  .redirectTo(MyTestPage)
-  .enterEmail('clement.guet@gmail.com');
+bescribe("Some description", function(context, describe, it) {
+  describe("My Test Page", function() {
+    it("has elements on it's page", function() {
+      context.Page.build()
+      .redirectTo(MyTestPage)
+      .clickOnUserButton()
+      .checkThatProfileIsShown();
+    });
+
+    // More it() functions can go here
+  });
+
+  // More describe() functions can go here
+});	
