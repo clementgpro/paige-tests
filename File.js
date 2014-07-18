3ceafd7f202de29610fabd43f75e33d9cef85569
Clement <clemzd@gmail.com>
2014-07-18 14:32:10 -0400

    the password and click button

--- File.js
+++ File.js
@@ -8,6 +8,18 @@ Home = Page.extend({
   selectors: {
     form: '.main #login-form',
     email: '.main #email',
+    password: '.main #pass',
+    button: '.main #send2'
+  },
+
+  clickButton: function() {
+    // Wait until the button is displayed
+    this.whenDisplayed(this.selectors.button).then(function() {
+      // click on the button
+      this.find(this.selectors.button).click();
+    }.bind(this));
+
+    return this;
   },
 
   // This can be accomplished using our Form component, but is included for illustration purposes.
@@ -16,7 +28,8 @@ Home = Page.extend({
     this.whenDisplayed(this.selectors.form).then(function () {
       // Enter the text in the text box
       this.find(this.selectors.email).sendKeys(text);
-      
+      this.find(this.selectors.password).sendKeys(text);
+      this.clickButton();
     }.bind(this));
 
     return this;
