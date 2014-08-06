0d563149c7eb600ca50fbe1f632a938d0f3bc390
Clement <clemzd@gmail.com>
2014-08-06 15:45:52 -0400

    sign-up
    test if the account has been well create

--- tests/spec/sign-up/sign-up.js
+++ tests/spec/sign-up/sign-up.js
@@ -1,6 +1,7 @@
 var bescribe = require('be-paige/bescribe');
 var TheQuizPage = require('../../lib/the-quiz/the-quiz.js');
 var SignUpPage = require('../../lib/sign-up/sign-up.js');
+var MonthlyBoxPage = require('../../lib/monthly-box/monthly-box.js');
 
 var config = {
   address: 'http://happy.pixafy.com/glassful/current/index.php/',
@@ -14,17 +15,22 @@ var config = {
 };
 
 bescribe("Sign up", config, function(context, describe, it) {
-  describe("Nominal", function() {
-    // login test
-    it("sign-up", function() {
+  describe("Accepted registration", function() {
+    it("Sign up successfully", function() {
       context.Page.build()
         .redirectTo(TheQuizPage)
         .goEndQuiz()
         .clickYesShippingUsa()
         .submitQuizForm()
         .switchTo(SignUpPage)
-        .completeSignUpForm('firstName', 'lastName', 'emailAddress', 'password')
-        .submitSignUpForm();
+        .completeSignUpForm(
+          'glassful',
+          'test',
+          'glassful.test' + new Date().getTime() + '@yopmail.com',
+          'password')
+        .submitSignUpForm()
+        .switchTo(MonthlyBoxPage)
+        .onPage();
     });
 
   });
